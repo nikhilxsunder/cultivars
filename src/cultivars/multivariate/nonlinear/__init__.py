e6322107f4ce74222570e8aba57cc096a7e88194
Nikhil Sunder <nsunder724@gmail.com>
2026-09-05 19:17:24 -0400

    add quantile and functional vars non linear complete
    Signed-off-by: Nikhil Sunder <nsunder724@gmail.com>

--- src/cultivars/multivariate/nonlinear/__init__.py
+++ src/cultivars/multivariate/nonlinear/__init__.py
@@ -1,1 +1,31 @@
 """Nonlinear multivariate models."""
+
+from .functional_coefficient import FunctionalCoefficientVAR, FunctionalCoefficientVARResult
+from .quantile import QVAR, QVARResult
+from .smooth_transition import STVAR, STVARResult
+from .threshold import TVAR, TVARResult
+from .time_varying import (
+    TVPVAR,
+    TVPVARSV,
+    TimeVaryingSVAR,
+    TVPSVARResult,
+    TVPVARResult,
+    TVPVARSVResult,
+)
+
+__all__ = [
+    "QVAR",
+    "STVAR",
+    "TVAR",
+    "TVPVAR",
+    "TVPVARSV",
+    "FunctionalCoefficientVAR",
+    "FunctionalCoefficientVARResult",
+    "QVARResult",
+    "STVARResult",
+    "TVARResult",
+    "TVPSVARResult",
+    "TVPVARResult",
+    "TVPVARSVResult",
+    "TimeVaryingSVAR",
+]
